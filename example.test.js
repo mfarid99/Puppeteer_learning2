e694b2720e57c9c37f80762dc4c94b7eeb8df083
Moe <moe@triblio.com>
2021-05-07 16:53:52 -0400

    2

--- example.test.js
+++ example.test.js
@@ -1,15 +1,30 @@
 const puppeteer = require('puppeteer')
 
-describe('My First Puppeteer Test', () => {
-    it('should launch the browser', async function(){
+// describe('My First Puppeteer Test', () => {
+//     it('should launch the browser', async function(){
+//         const browser = await puppeteer.launch({headless: false, devtools: true})
+//         const page = await browser.newPage()
+//         await page.goto('http://www.example.com/')
+//         await page.waitForTimeout(1000)
+//         await page.waitForSelector('h1')
+//         await page.reload()
+//         await page.waitForTimeout(1000)
+//         await page.waitForSelector('h1')
+//         await browser.close()
+//     })
+    
+// }, 120000)
+
+describe('My Second Puppeteer Test', async () => {
+    it('Should launch another browser', async () => {
         const browser = await puppeteer.launch({headless: false})
         const page = await browser.newPage()
-        await page.goto('http://www.example.com/')
-        await page.waitForSelector('h1')
-        await page.waitForSelector('div')
-        await page.waitForSelector('body')
-        await page.waitForSelector('p')
+
+        await page.goto('https://www.autodesk.com/products/maya/overview?term=1-YEAR')
         await browser.close()
+
     })
-    
-})
+   
+
+
+}, 120000)
